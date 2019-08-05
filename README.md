1e50cbc3c0bbff5beeded6778416fa5dde3fbda0
michael <mj@hph.io>
2019-08-05 10:07:41 -0400

    Added conf file and various switches to help searches for bounced, delievered, and whitelisted emails

--- README.md
+++ README.md
@@ -1,11 +1,42 @@
 # Search Email Logs
 
+## Summary
 This script grabs all the available logs on a mail server, concatenates them into a single file for search - gunzipping
 where appropriate.
 
-Syntax: search-email-logs [search string]
+Once the script runs, it creates a directory: `/tmp/maillog`, which it uses for operations.
+Inside that directory, it creates `search.log`, which is the concatenated log files pulled for use.
+
+Subsequent searches will use the `search.log` file by default. So, if you need to reload or refresh the data,
+use the `-r` option.
+
+## Options
+
+Syntax:
+  search-email-logs.sh [option] [string]
+
+Options:
+  -r        Reload and reconcatenated the log files.
+  -d        Search for delivered emails (Status=250 OK)
+  -w        Search for whitelisted emails
+  -j        Search for rejected emails
+  -h        Show this help screen.
+
+## Example
 
 The example below searches all available email logs for `example.org`
 ```
 search-email-logs example.org
+```
+
+## Configuration
+
+The `/etc/mailsearch/mailsearch.conf` can be used to set various settings. As of now, they are:
+
+### WHITELISTSTRING
+
+Define what string constitutes a whitelist accept.
+Example:
+```
+WHITELISTSTRING=triggers FILTER whitelist
 ```
--- search-email-logs.sh
+++ search-email-logs.sh
@@ -3,15 +3,16 @@ MAILLOG=/tmp/maillog
 UNZIPQUEUE=${MAILLOG}/unzip.queue
 ZIPPEDLIST=${MAILLOG}/zipped.list
 SEARCHLOG=${MAILLOG}/search.log
+RESULTS=${MAILLOG}/results.txt
+CONF=/etc/mailsearch/mailsearch.conf
 
-echo "Using:" 
-echo "Mail log directory: ${MAILLOG}"
-echo "Zipped list: ${ZIPPEDLIST}"
-echo "Unzip queue: ${UNZIPQUEUE}"
-
-if [ ! -d ${MAILLOG} ]; then
-	mkdir ${MAILLOG}
+if [ -f ${CONF} ]; then
+    source ${CONF}
+else
+    WHITELISTSTRING="Sender address triggers FILTER relay:192.168.3.81"
 fi
+
+function reconcat() {
 
 cd ${MAILLOG}
 rm -vfr ${MAILLOG}/*
@@ -32,4 +33,93 @@ done < ${UNZIPQUEUE}
 cat /var/log/mail.log.1 >> ${SEARCHLOG}
 cat /var/log/mail.log >> ${SEARCHLOG}
 
-grep $1 ${SEARCHLOG}
+}
+
+function showhelp() {
+cat <<EOF
+SEARCH EMAIL LOGS
+
+Searches email logs (gunzipping and concatenating as necessary) for a given string and disposition.
+
+Syntax:
+  search-email-logs.sh [option] [string]
+
+Options:
+  -r        Reload and reconcatenated the log files.
+  -d        Search for delivered emails (Status=250 OK)
+  -w        Search for whitelisted emails
+  -j        Search for rejected emails
+  -h        Show this help screen.
+
+File support issues at: https://github.com/mjmunger/search-mail-logs
+
+EOF
+
+}
+
+function showresults() {
+
+    cat ${RESULTS}
+    #Show count
+    COUNT=$(cat ${RESULTS} | wc -l)
+    echo ""
+    echo "${COUNT} hits"
+}
+
+function checkarg() {
+    if [ -z $1 ]; then
+        errorout $2
+    fi
+}
+
+function errorout() {
+    echo $1
+    exit
+}
+
+function specialsearch() {
+    SEARCHTERM=$1
+    FILTER=$2
+    grep "${SEARCHTERM}" ${SEARCHLOG} | grep "${FILTER}" > ${RESULTS}
+}
+
+echo "Using:"
+echo "Mail log directory: ${MAILLOG}"
+echo "Zipped list: ${ZIPPEDLIST}"
+echo "Unzip queue: ${UNZIPQUEUE}"
+
+if [ ! -d ${MAILLOG} ]; then
+	mkdir ${MAILLOG}
+	reconcat
+fi
+
+cat /dev/null > ${RESULTS}
+
+case $1 in
+    "-r")
+        reconcat
+        echo "All mail files concatenated to ${SEARCHLOG}"
+        exit
+        ;;
+    "-j")
+        checkarg $2 "When searching for rejections, use this syntax: 'search-email-logs.sh -j [search string]'"
+        specialsearch $2 'reject'
+        ;;
+    "-d")
+        checkarg $2 "When searching for delivered emails, use this syntax: 'search-email-logs.sh -d [search string]'"
+        specialsearch $2 '250'
+        ;;
+    "-w")
+        checkarg $2 "When searching for emails delivered via whitelist, use this syntax: 'search-email-logs.sh -w [search string]'"
+        specialsearch $2 ${WHITELISTSTRING}
+        ;;
+    "-h")
+        showhelp
+        exit
+        ;;
+    *)
+        grep $1 ${SEARCHLOG} > ${RESULTS}
+        ;;
+esac
+
+showresults
